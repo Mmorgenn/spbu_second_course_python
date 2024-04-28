ff6433b02adf0e8715f12c0646771273f2a1c513
Mmorgenn <marknsya951@gmail.com>
2024-04-28 19:00:50 +0300

    Fix: Redistributed the methods

--- src/Homework/Homework_1/treap.py
+++ src/Homework/Homework_1/treap.py
@@ -1,8 +1,9 @@
+from copy import deepcopy
 from random import random
 from typing import Generic, Iterator, MutableMapping, Optional, TypeVar
 
 Value = TypeVar("Value")
-Key = TypeVar("Key")
+Key = TypeVar("Key", int, str)
 
 
 class Node(Generic[Key, Value]):
@@ -10,15 +11,15 @@ class Node(Generic[Key, Value]):
         self.key: Key = key
         self.value: Value = value
         self.priority: float = random()
-        self.left: Node | None = None
-        self.right: Node | None = None
+        self.left: Optional["Node"] = None
+        self.right: Optional["Node"] = None
         self.size: int = 1
 
     def __eq__(self, other_node: object) -> bool:
         return isinstance(other_node, Node) and (self.key, self.value) == (other_node.key, other_node.value)
 
     def __str__(self) -> str:
-        return f"Node(value={self.value}, key={self.key}, priority={self.priority}, left={self.left}, right={self.right}, size={self.size})"
+        return f"Node('key': {self.key}, 'value': {self.value})"
 
     def __repr__(self) -> str:
         return f"{self.__dict__}"
@@ -29,47 +30,98 @@ class Node(Generic[Key, Value]):
             return 0
         return node.size
 
-    @staticmethod
-    def update_size(node: Optional["Node"]) -> Optional["Node"]:
-        if node is not None:
-            node.size = Node.get_size(node.left) + Node.get_size(node.right) + 1
-        return node
+    def update_size(self) -> None:
+        self.size = Node.get_size(self.left) + Node.get_size(self.right) + 1
+
+    def split(self, key: Key, deepcopy_on: bool = False) -> tuple[Optional["Node"], Optional["Node"]]:
+
+        def _split(node: Optional["Node"]) -> tuple[Optional["Node"], Optional["Node"]]:
+            if node is None:
+                return None, None
+            if key > node.key:
+                node_first, node_second = _split(node.right)
+                node.right = node_first
+                node.update_size()
+                return node, node_second
+            else:
+                node_first, node_second = _split(node.left)
+                node.left = node_second
+                node.update_size()
+                return node_first, node
+
+        if deepcopy_on:
+            return _split(deepcopy(self))
+        return _split(self)
+
+    def merge(self, other: Optional["Node"], deepcopy_on: bool = False) -> "Node":
+
+        def _merge(node: "Node", other: Optional["Node"]) -> "Node":
+            if other is None:
+                return node
+            if node.priority > other.priority:
+                if node.right is None:
+                    node.right = other
+                else:
+                    node.right = _merge(node.right, other)
+                node.update_size()
+                return node
+            else:
+                other.left = _merge(node, other.left)
+                other.update_size()
+                return other
+
+        if deepcopy_on:
+            return _merge(deepcopy(self), deepcopy(other))
+        return _merge(self, other)
+
+    def _del_smallest(self) -> Optional["Node"]:
+
+        def recursion_del(node: Node) -> Node | None:
+            if node.left is None:
+                return node.right
+            node.left = recursion_del(node.left)
+            node.update_size()
+            return node
+
+        return recursion_del(self)
 
 
 class Treap(MutableMapping):
-    def __init__(self) -> None:
-        self.root: Node | None = None
+    def __init__(self, root: Node | None = None) -> None:
+        self.root: Node | None = root
 
     def __getitem__(self, key: Key) -> Value | None:
-
-        def recursion_find(node: Node | None) -> Value:
-            if node is None:
-                raise KeyError("No such key")
-            if node.key == key:
-                return node.value
-            if node.key > key:
-                return recursion_find(node.left)
-            else:
-                return recursion_find(node.right)
-
-        return recursion_find(self.root)
+        node = self._get_node(key)
+        if node is None:
+            raise KeyError("There is no such key!")
+        return node.value
 
     def __setitem__(self, key: Key, value: Value) -> None:
         if self._has_key(key):
             raise KeyError("Such key is already added")
-        tree_additional = Treap()
         node_additional = Node(key, value)
-        tree_additional.root = node_additional
-        tree_smaller, tree_bigger = self._split(key)
-        tree_with_key = Treap._merge(tree_smaller, tree_additional)
-        self.root = Treap._merge(tree_with_key, tree_bigger).root
+        if self.root is None:
+            self.root = node_additional
+            return
+        node_smaller, node_bigger = self.root.split(key)
+        if node_smaller is None:
+            self.root = node_additional.merge(node_bigger)
+            return
+        node_with_key = node_smaller.merge(node_additional)
+        self.root = node_with_key.merge(node_bigger)
 
     def __delitem__(self, key: Key) -> None:
-        if not self._has_key(key):
+        if self.root is None or not self._has_key(key):
             raise KeyError("There is no such key")
-        tree_smaller, tree_bigger = self._split(key)
-        tree_bigger._del_smaller()
-        self.root = Treap._merge(tree_smaller, tree_bigger).root
+        node_smaller, node_bigger = self.root.split(key)
+        if node_bigger is None:
+            # Otherwise mypy will not understand
+            raise KeyError("There is no such key")
+        node_bigger = node_bigger._del_smallest()
+        if node_smaller is None:
+            self.root = node_bigger
+            return
+        self.root = node_smaller.merge(node_bigger)
 
     def __iter__(self) -> Iterator[Value]:
         keys = []
@@ -91,73 +143,25 @@ class Treap(MutableMapping):
         return Node.get_size(self.root)
 
     def __str__(self) -> str:
-        return f"Treap(root={self.root})"
+        return f"Treap{str(self.items())[9:]}"
 
     def __repr__(self) -> str:
         return f"{self.__dict__}"
 
-    @staticmethod
-    def _split_node(node: Node | None, key: Key) -> tuple[Optional[Node], Optional[Node]]:
-        if node is None:
-            return None, None
-        if key > node.key:
-            node_first, node_second = Treap._split_node(node.right, key)
-            node.right = node_first
-            return Node.update_size(node), node_second
-        else:
-            node_first, node_second = Treap._split_node(node.left, key)
-            node.left = node_second
-            return node_first, Node.update_size(node)
+    def _get_node(self, key: Key) -> Node | None:
 
-    def _split(self, key: Key) -> tuple["Treap", "Treap"]:
-        node_first, node_second = Treap._split_node(self.root, key)
-        tree_first, tree_second = Treap(), Treap()
-        tree_first.root = node_first
-        tree_second.root = node_second
-        return tree_first, tree_second
-
-    @staticmethod
-    def _merge_node(node_first: Node | None, node_second: Node | None) -> Node | None:
-        if node_first is None:
-            return node_second
-        if node_second is None:
-            return node_first
-        if node_first.priority > node_second.priority:
-            node_first.right = Treap._merge_node(node_first.right, node_second)
-            return Node.update_size(node_first)
-        else:
-            node_second.left = Treap._merge_node(node_first, node_second.left)
-            return Node.update_size(node_second)
-
-    @staticmethod
-    def _merge(tree_first: "Treap", tree_second: "Treap") -> "Treap":
-        tree_new = Treap()
-        node_new = Treap._merge_node(tree_first.root, tree_second.root)
-        tree_new.root = node_new
-        return tree_new
-
-    def _del_smaller(self) -> None:
-        if self.root is None:
-            return
-
-        def recursion_del(node: Node) -> Node | None:
-            if node.left is None:
-                return node.right
-            node.left = recursion_del(node.left)
-            return Node.update_size(node)
-
-        self.root = recursion_del(self.root)
-
-    def _has_key(self, key: Key) -> bool:
-
-        def recursion_find(node: Node | None) -> bool:
+        def recursion_find(node: Node | None) -> Node | None:
             if node is None:
-                return False
+                return None
             if node.key == key:
-                return True
+                return node
             if node.key > key:
                 return recursion_find(node.left)
             else:
                 return recursion_find(node.right)
 
         return recursion_find(self.root)
+
+    def _has_key(self, key: Key) -> bool:
+        node = self._get_node(key)
+        return not (node is None)
