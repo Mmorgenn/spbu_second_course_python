da0497eed0f195b88c5f7ff5b57ec8a2fe80db2c
Mmorgenn <marknsya951@gmail.com>
2024-04-29 13:33:25 +0300

    Upd: Improve tests

--- tests/Homework/Homework_3/test_orm.py
+++ tests/Homework/Homework_3/test_orm.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from json import loads
 
 import pytest
 
@@ -48,32 +49,32 @@ def test_get_item(orm, data, keys, expected):
 
 
 @pytest.mark.parametrize(
-    "orm,data",
+    "orm,data,expected",
     (
-        (Product(), {"name": "apple", "price": 10, "rating": 5, "count": 100}),
-        (Product(), {"name": "banana", "price": 25, "rating": 4, "count": 123, "type": "fruit"}),
-        (Weather(), {"temp": 12.5, "wind_speed": 10.5, "humidity": 66}),
+        (Product(), {"name": "apple", "price": 10, "rating": 5, "count": 100}, {"name": "apple", "price": 10, "rating": 5, "count": 100}),
+        (Product(), {"name": "banana", "price": 25, "rating": 4, "count": 123, "type": "fruit"}, {"name": "banana", "price": 25, "rating": 4, "count": 123}),
+        (Weather(), {"temp": 12.5, "wind_speed": 10.5, "humidity": 66}, {"temp": 12.5, "wind_speed": 10.5, "humidity": 66}),
     ),
 )
-def test_dump(orm, data):
+def test_dump(orm, data, expected):
     current_orm = orm.upload_dict(data)
     result = current_orm.dump()
-    assert result is not None
+    assert loads(result) == expected
 
 
 @pytest.mark.parametrize(
-    "orm,data,key,value",
+    "orm,data,key,value,expected",
     (
-        (Product(), {"name": "apple", "price": 10, "rating": 5, "count": 100}, "rating", 1),
-        (Product(), {"name": "banana", "price": 25, "rating": 4, "count": 123, "type": "fruit"}, "count", 90),
-        (Weather(), {"temp": 12.5, "wind_speed": 10.5, "humidity": 66}, "temp", 0.0),
+        (Product(), {"name": "apple", "price": 10, "rating": 5, "count": 100}, "rating", 1, {"name": "apple", "price": 10, "rating": 1, "count": 100}),
+        (Product(), {"name": "banana", "price": 25, "rating": 4, "count": 123, "type": "fruit"}, "count", 90, {"name": "banana", "price": 25, "rating": 4, "count": 90}),
+        (Weather(), {"temp": 12.5, "wind_speed": 10.5, "humidity": 66}, "temp", 0.0, {"temp": 0.0, "wind_speed": 10.5, "humidity": 66}),
     ),
 )
-def test_dump_changed(orm, data, key, value):
+def test_dump_changed(orm, data, key, value, expected):
     current_orm = orm.upload_dict(data)
     setattr(current_orm, key, value)
     result = current_orm.dump()
-    assert result is not None
+    assert loads(result) == expected
 
 
 @pytest.mark.parametrize(
