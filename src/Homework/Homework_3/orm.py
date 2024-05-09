f02b7f0d9ecaeae742bd735096f2ef5b90fa7b8b
Mmorgenn <marknsya951@gmail.com>
2024-05-09 03:49:14 +0300

    Fix: change data attribute

--- src/Homework/Homework_3/orm.py
+++ src/Homework/Homework_3/orm.py
@@ -14,11 +14,11 @@ class CustomDescriptor(Generic[T]):
         self.default = default
 
     def __get__(self, instance: T, owner: Type[T]) -> Any:
-        if not hasattr(instance, "data"):
+        if not hasattr(instance, "jdata"):
             raise JsonError("Json data is missing")
         value = getattr(instance, f"_{self.default}", self.default)
         if value is None:
-            new_value = instance.data.get(self.default, None)
+            new_value = instance.jdata.get(self.default, None)
             if new_value:
                 setattr(instance, self.default, new_value)
                 return new_value
@@ -41,7 +41,7 @@ class ORM(Generic[T]):
     def upload_dict(cls: Type[T], data: dict) -> T:
         new_cls = cls.apply_descriptor()
         result = new_cls()
-        setattr(result, "data", data)
+        setattr(result, "jdata", data)
         return result
 
     def dump(self) -> str | None:
